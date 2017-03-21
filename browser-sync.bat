52cdf4b8f4db565bdecfdeec9f1074f4d1f6caf6
brianchristopherbrady <brian.christopher.brady@gmail.com>
2017-03-21 09:46:07 -0700

    removes https://

--- browser-sync.bat
+++ browser-sync.bat
@@ -1,4 +1,4 @@
 SET /P BsUrl=Enter URL to test with BrowserSync:
 ECHO: "OPENING %BsUrl% WITH BROWSER-SYNC"
-browser-sync start --proxy "https://"%BsUrl% --files "content/*css, scripts/*.js, views/**/*.cshtml" --open ui
+browser-sync start --proxy %BsUrl% --files "content/*css, scripts/*.js, views/**/*.cshtml" --open ui
 PAUSE
